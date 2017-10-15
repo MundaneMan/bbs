9f645b1e6027cf359e8949748088e6296d8822f9
libinghope <libinghope@gmail.com>
2017-10-15 16:02:16 +0800

    给发帖增加添加表情的功能

--- assets/home/js/ckeditor/config.js
+++ assets/home/js/ckeditor/config.js
@@ -24,7 +24,7 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'colors' },
 		// { name: 'about' }
 	];
-	config.extraPlugins = "colorbutton,font";
+	config.extraPlugins = "colorbutton,font,smiley";
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
